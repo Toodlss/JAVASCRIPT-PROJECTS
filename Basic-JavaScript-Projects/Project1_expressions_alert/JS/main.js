26293682bbe8bf66459af4827aa98be3523e2775
Kaden M Bilyeu <kadenbilyeu@yahoo.com>
2026-02-14 16:13:17 -0800

    java

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,21 @@
-window.alert("hello,world!");
-document.write("hello,world!");
+//popup message when the page loads
+window.alert("Welcome to my JavaScript project!");
 
-var A ="this is a string";
-document.write(A);
+// Creating a variable named Sent1 and assigning it a string value
+var Sent1 = "This is the beginning of the string";
+
+// Creating a variable named Sent2 and assigning it a string value
+var Sent2 = " and this is the end of the string";
+
+// Concatenating (joining) Sent1 and Sent2 together
+var combinedSentence = Sent1 + Sent2;
+
+// Writing the concatenated sentence to the webpage
+document.write(combinedSentence);
+
+// Writing a line break for better formatting
+document.write("<br>");
+
+// Writing an expression (math operation) to the webpage
+// This expression adds two numbers together
+document.write("5 + 10 = " + (5 + 10));
