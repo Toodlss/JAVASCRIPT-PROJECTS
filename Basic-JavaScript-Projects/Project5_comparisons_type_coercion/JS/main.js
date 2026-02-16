dff5401ae6168f873f696ef6ccb01ea74605c6e5
Kaden M Bilyeu <kadenbilyeu@yahoo.com>
2026-02-16 08:18:53 -0800

    java

---
+++ Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,10 @@
+// Display NaN
+document.write(0 / 0);
+document.write("<br>");
+
+// Display true
+document.write(isNaN("Hello"));
+document.write("<br>");
+
+// Display false
+document.write(isNaN("123"));
